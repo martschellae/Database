6598ebe0c58ef75935e5507ee775df74064985c0
Marcel Rupp <marcel_rupp@t-online.de>
2021-03-29 08:12:05 +0200

    Pixel and github webhook test

--- keygen.h
+++ keygen.h
@@ -76,3 +76,67 @@ std::string createRandomKey(std::string base, unsigned int length = 128) {
 	}
 	return key;
 }
+void encryptPicture(std::string path, std::string key) {
+	std::string keyword = key;
+	sf::Image old;
+	if (!old.loadFromFile(path)) {
+		std::cout << "datei error";
+	}
+	while (keyword.size() < (old.getSize().x * old.getSize().y * 3)) {
+		keyword += keyword;
+	}
+	sf::Image new_Image;
+	new_Image.create(old.getSize().x, old.getSize().y);
+	
+	int c = 0;
+	for (int y = 0; y < old.getSize().y; y++) {
+		for (int x = 0; x < old.getSize().x; x++) {
+			//sf::Color worker;
+			/*worker = old.getPixel(x, y);
+			worker.r = (worker.r - keyword[c]) % 255;
+			worker.r = (worker.r + keyword[c]) % 255;
+			c++;
+			worker.g = (worker.g - keyword[c]) % 255;
+			worker.g = (worker.g + keyword[c]) % 255;
+			c++;
+			worker.b = (worker.b - keyword[c]) % 255;
+			worker.b = (worker.b + keyword[c]) % 255;
+			c++;
+			*/
+			
+			new_Image.setPixel(x, y, { (UINT8)(old.getPixel(x, y).r + keyword[c]), (UINT8)(old.getPixel(x, y).g + keyword[c]), (UINT8)(old.getPixel(x, y).b + keyword[c])});
+			c++;
+		}
+	}
+	new_Image.saveToFile(path);
+}
+void decryptPicture(std::string path, std::string key) {
+	std::string keyword = key;
+	sf::Image old;
+	if (!old.loadFromFile(path)) {
+		std::cout << "datei error";
+	}
+	while (keyword.size() < (old.getSize().x * old.getSize().y * 3)) {
+		keyword += keyword;
+	}
+
+	sf::Image new_Image;
+	new_Image.create(old.getSize().x, old.getSize().y, {0, 0, 0});
+	
+	int c = 0;
+	for (int y = 0; y < old.getSize().y; y++) {
+		for (int x = 0; x < old.getSize().x; x++) {
+			sf::Color worker;
+			worker = old.getPixel(x, y);
+			worker.r = (worker.r + keyword[c] * 16) % 255;
+			c++;
+			worker.g = (worker.g + keyword[c] * 16) % 255;
+			c++;
+			worker.b = (worker.b + keyword[c] * 16) % 255;
+			c++;
+			worker.a = 255;
+			new_Image.setPixel(x, y, worker);
+		}
+	}
+	new_Image.saveToFile(path);
+}
