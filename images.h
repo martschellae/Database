9c00540a858cf180a6bf854015b15ac45525c942
Marcel Rupp <marcel_rupp@t-online.de>
2021-04-05 20:58:42 +0200

    limiter ist drinnen, als nächstes diesen blöden balken weg

--- images.h
+++ images.h
@@ -13,84 +13,6 @@
 #include "templates.h"
 #include "networking.h"
 
-class ParticleSystem : public sf::Drawable, public sf::Transformable
-{
-public:
-
-    ParticleSystem(unsigned int count) :
-        m_particles(count),
-        m_vertices(sf::Points, count),
-        m_lifetime(sf::seconds(3.f)),
-        m_emitter(0.f, 0.f)
-    {
-    }
-
-    void setEmitter(sf::Vector2f position)
-    {
-        m_emitter = position;
-    }
-
-    void update(sf::Time elapsed)
-    {
-        for (std::size_t i = 0; i < m_particles.size(); ++i)
-        {
-            // update the particle lifetime
-            Particle& p = m_particles[i];
-            p.lifetime -= elapsed;
-
-            // if the particle is dead, respawn it
-            if (p.lifetime <= sf::Time::Zero)
-                resetParticle(i);
-
-            // update the position of the corresponding vertex
-            m_vertices[i].position += p.velocity * elapsed.asSeconds();
-
-            // update the alpha (transparency) of the particle according to its lifetime
-            float ratio = p.lifetime.asSeconds() / m_lifetime.asSeconds();
-            m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
-        }
-    }
-
-private:
-
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-    {
-        // apply the transform
-        states.transform *= getTransform();
-
-        // our particles don't use a texture
-        states.texture = NULL;
-
-        // draw the vertex array
-        target.draw(m_vertices, states);
-    }
-
-private:
-
-    struct Particle
-    {
-        sf::Vector2f velocity;
-        sf::Time lifetime;
-    };
-
-    void resetParticle(std::size_t index)
-    {
-        // give a random velocity and lifetime to the particle
-        float angle = (std::rand() % 360) * 3.14f / 180.f;
-        float speed = (std::rand() % 50) + 50.f;
-        m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-        m_particles[index].lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
-
-        // reset the position of the corresponding vertex
-        m_vertices[index].position = m_emitter;
-    }
-
-    std::vector<Particle> m_particles;
-    sf::VertexArray m_vertices;
-    sf::Time m_lifetime;
-    sf::Vector2f m_emitter;
-};
-
 void imageInterface() {
 
     showConsole();
@@ -131,18 +53,6 @@ void imageInterface() {
     displayTexture.loadFromFile("assets/image.jpg");
     displayTexture.update(image);
 
-
-    sf::Color pick = sf::Color::Red;
-    sf::VertexArray colorPalette(sf::Quads, 4);
-    colorPalette[0].color = sf::Color::White;
-    colorPalette[0].position = { 0, 0 };
-    colorPalette[1].color = pick;
-    colorPalette[1].position = { 1000, 0 };
-    colorPalette[2].color = pick;
-    colorPalette[2].position = { 1000, 1000 };
-    colorPalette[3].color = sf::Color::Black;
-    colorPalette[3].position = { 0, 1000 };
-
     sf::Sprite display;
     display.setTexture(displayTexture);
     display.setPosition(sf::Vector2f(0, 0));
@@ -182,19 +92,10 @@ void imageInterface() {
                // image.setPixel((UINT)mouseOnImagePos.x, (UINT)mouseOnImagePos.y, sf::Color::Black);
             //}
             if (event.type == sf::Event::MouseWheelScrolled) {
+                if((zoom + 0.05 * event.mouseWheelScroll.delta) > 0.25 && (zoom + 0.05 * event.mouseWheelScroll.delta) < 2)
                 zoom += 0.05 * event.mouseWheelScroll.delta;
+            
             }
-        }
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-            image.setPixel((UINT)mouseOnImagePos.x, (UINT)mouseOnImagePos.y-1, sf::Color::Black);
-            image.setPixel((UINT)mouseOnImagePos.x-1, (UINT)mouseOnImagePos.y, sf::Color::Black);
-            image.setPixel((UINT)mouseOnImagePos.x-1, (UINT)mouseOnImagePos.y-1, sf::Color::Black);
-            image.setPixel((UINT)mouseOnImagePos.x-1, (UINT)mouseOnImagePos.y+1, sf::Color::Black);
-            image.setPixel((UINT)mouseOnImagePos.x, (UINT)mouseOnImagePos.y, sf::Color::Black);
-            image.setPixel((UINT)mouseOnImagePos.x+1, (UINT)mouseOnImagePos.y - 1, sf::Color::Black);
-            image.setPixel((UINT)mouseOnImagePos.x+1, (UINT)mouseOnImagePos.y+1, sf::Color::Black);
-            image.setPixel((UINT)mouseOnImagePos.x+1, (UINT)mouseOnImagePos.y, sf::Color::Black);
-            image.setPixel((UINT)mouseOnImagePos.x, (UINT)mouseOnImagePos.y+1, sf::Color::Black);
         }
         imagePosition = { (float)window.getSize().x / 2 - (float)displayTexture.getSize().x * display.getScale().x / 2, (float)window.getSize().y / 2 - (float)displayTexture.getSize().y * display.getScale().y / 2 };
 
@@ -208,7 +109,6 @@ void imageInterface() {
 
         window.draw(display);
         window.draw(menuBar);
-        //window.draw(colorPalette);
         window.display();
     }
 }
